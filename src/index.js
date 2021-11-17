9d3efe6413d22b2f7820b53901a1481442da9a1b
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-17 14:13:57 +0200

    Lesson 11 - Switch Statements.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,25 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // ----
 
 const number = 1;
-const result = 'The number is: ' + (number === 1 ? 'One' : 'No-Match');
+let result; // undefined
+
+switch (number) {
+    case 1: {
+        const text = 'One';
+        result = text;
+        break;
+    }
+    case 99: {
+        const text = 'Ninety-Nine';
+        result = text;
+        break;
+    }
+    case 1000:
+        result = 'One-Thousand'
+        break;
+    default:
+        result = 'No-Match';
+        break;
+}
 
 console.log(result);
