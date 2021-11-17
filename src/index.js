68674bb31b11560797195a31b9c4a3ac75877573
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-17 16:11:04 +0200

    Lesson 17 - Correctly Type-Checking Numbers.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-const immutableNumber = 99;
-let referencedNumber = immutableNumber;
+console.log(typeof 99);                           // number
+console.log(typeof 99.66);                        // number
+console.log(99 instanceof Number);                // false
+console.log(Number('99') instanceof Number);      // false
+console.log(new Number('99') instanceof Number);  // true
 
-referencedNumber = 44;
-
-console.log(immutableNumber);
-console.log(referencedNumber);
+console.log({}.toString());                       // [object Object]
+console.log(Object.prototype.toString());         // [object Object]
+console.log(Object.prototype.toString.call(99));  // [object Number], this is the safe way to detect if something is of a particular type
+console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number');  // true
+console.log(Object.prototype.toString.call(99) === '[object Number]');      // true
