2b21a3b7ec8790b841587f4c630e0b61f9415324
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-19 15:27:40 +0200

    Lesson 20 - String Properties and Indexes.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,13 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-// literal syntax
-console.log('Pizza');                             // Pizza string literal
-console.log("Pizza");                             // Pizza string literal
+console.log(new String(5599));
+console.log(new String(5599).length);
+console.log(new String(5599), 'Pepperoni'.length);
 
-console.log("Pizza 'Hello!'");                    // Pizza 'Hello!'
-console.log('Pizza \'Hello!\'');                  // Pizza 'Hello!'
-console.log('Pizza "Hello!"');                    // Pizza "Hello!"
+console.log('Pepperoni'[0]);       // P
+console.log('Pepperoni'['0']);     // P
+console.log('Pepperoni'[1]);       // e
 
-const pizza = 'Pepperoni';
-console.log('Pizza is: ' + pizza + '!');          // Pizza is: Pepperoni!
-console.log(`Pizza is: ${pizza}!`);               // Pizza is: Pepperoni!, new syntax from ES2015
-console.log(`
-Pizza is: 
-${pizza}!
-`);
-
-// function syntax
-console.log(String(55 + 11), String(55 + '11'));  // 66 5511
-console.log(String({ name: 'Pepperoni'}));        // [object Object]
-console.log(String([1, 2, 3, 4]));                // 1,2,3,4
-console.log(String([1, 2, 3, 4]).toString());     // 1,2,3,4
-
-// constructor syntax, creates a wrapper Object
-// avoid using it
-console.log(new String(55));                      // String {'55'}
+const pizza = 'P e p p e r o n i';
+console.log(pizza[0], pizza[pizza.length - 1], pizza.length);  // P i 17
