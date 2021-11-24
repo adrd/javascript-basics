5a56172172f11681bb4222642d7d25ab51e44a63
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-24 11:44:48 +0200

    Lesson 24 - Boolean Literal, Function and Constructor Syntax.

--- src/index.js
+++ src/index.js
@@ -5,33 +5,19 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-console.log(String.prototype);
+console.log(true, false);
 
-console.log('Pepperoni'.indexOf('P'));   // 0
-console.log('Pepperoni'.indexOf('z'));   // -1
+const arg = '';
+console.log(Boolean(arg));       // false
 
-// if ('Pepperoni'.indexOf('P') !== -1) {
+const arg1 = 'X';
+console.log(Boolean(arg1));      // true
 
-// }
+console.log(!!arg1);             // true
 
-console.log('Pepperoni'.includes('P'));                             // true, new in ES2015
+console.log(Boolean([]));        // true
+console.log(Boolean({}));        // true
 
-console.log('Pepperoni'.replace('oni', 'fire'));                    // Pepperfire
-console.log('Pepperoni'.replace(/oni$/, 'fire'));                   // Pepperfire, using Regular Expressions
+console.log(new Boolean(arg1));  // Boolean {true}
 
-console.log('Pepperoni'.slice(2, -1));                              // pperon
-console.log('Pepperoni'.slice(0, -1));                              // Pepperon
-console.log('Pepperoni'.slice(0, -2));                              // Peppero
-console.log('Pepperoni'.slice(0, -3));                              // Pepper
-console.log('Pepperoni'.slice(0, 1));                               // P
-console.log('Pepperoni'.slice(0, 'Pepperoni'.length));              // Pepperoni
 
-console.log('Pepperoni~Plain'.split('~'));                          // (2) ['Pepperoni', 'Plain']
-console.log('Pepperoni~Plain'.split('~')[0]);                       // Pepperoni
-console.log('Pepperoni~Plain'.split(/~/)[0]);                       // Pepperoni, using Regular Expressions
-
-console.log('   4444 5555 6666 7777 ');                             //    4444 5555 6666 7777 
-console.log('   4444 5555 6666 7777 '.trim());                      // 4444 5555 6666 7777
-console.log('   4444 5555 6666 7777 '.length);                      // 23
-console.log('   4444 5555 6666 7777 '.trim().length);               // 19
-console.log('   4444 5555 6666 7777 '.trim().replace(/\s/g, ''));   // Regular Expression for removing whitespace
