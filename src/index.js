cdae1b807297320cff176be149476e5595b7c4a4
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-24 12:52:40 +0200

    Lesson 26 - Function Declarations and Expressions.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,39 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-// true = literal Boolean
-// false = literal Boolean
-console.log(typeof true);                          // boolean, gives back literal String representation of Boolean
-console.log(typeof false);                         // boolean, gives back literal String representation of Boolean
+// hoisting
+console.log(makeCar);                  // makeCar function is available, applies toString() to function object/instance and displays the function code
+console.log(makeCarExpression);        // undefined
+console.log(makeCarExpression1);       // undefined
+console.log(makeCarArrow);             // undefined
+console.log(makeCarArrowShorthand);    // undefined
 
-console.log(true instanceof Boolean);              // false, gives back literal Boolean
+// 1: Function Declaration
+function makeCar() {
+    console.log('Making a car...');
+}
 
-console.log(new Boolean('') instanceof Boolean);   // true, gives back literal Boolean
-console.log(new Boolean('X') instanceof Boolean);  // false, gives back literal Boolean
+console.log(makeCar);    // applies toString() to function object/instance and displays the function code
 
-console.log(Object.prototype.toString.call(true)); // [object Boolean], this is the safe way to detect if something is of a particular type
+makeCar();
+
+// 2: Function Expression (anonymous or named)
+// const makeCarExpression = function makeCar() {};
+const makeCarExpression = function () {};             // this is an example of an anonymous function
+console.log(makeCarExpression.name);                  // makeCarExpression, returns const name, which is considered to be function name, 
+                                                      
+const makeCarExpression1 = function myFunction() {};
+console.log(makeCarExpression1.name);                 // myFunction, returns function name 
+
+console.log(makeCar.name);                            // makeCar, returns function name
+
+// 3: Arrow Function (new to ES2015)
+const makeCarArrow = () => {
+    console.log('Making car inside Arrow...');
+};
+
+makeCarArrow();
+
+const makeCarArrowShorthand = () => console.log('Short');
+
+makeCarArrowShorthand();
