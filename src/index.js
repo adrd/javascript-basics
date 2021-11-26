9e722f76028faa314d05360781739cd624b77d09
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 18:04:13 +0200

    Lesson 43 - Correctly Type-Checking Objects.

--- src/index.js
+++ src/index.js
@@ -8,21 +8,32 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 const drink = {
     id: 'xhs8Pla',
     name: 'Lemonade',
+    price: {
+        sale: 99,
+        full: 129
+    },
 };
 
-const price = {
-    sale: 99,
-    full: 129
+// this is not reliable
+console.log(typeof drink);           // object
+console.log(typeof []);              // object
+console.log(typeof null)             // object
+
+// this is not reliable
+console.log({} instanceof Object);   // true
+console.log([] instanceof Object);   // true
+console.log(null instanceof Object); // false
+
+// this is reliable
+console.log(Object.prototype.toString.call(drink));    // [object Object]
+console.log(Object.prototype.toString.call(null));     // [object Null]
+console.log(Object.prototype.toString.call([]));       // [object Array]
+
+function getType(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
 }
 
-const mergedDrink = Object.assign({}, drink, price);
-
-console.log(drink, price);            // {id: 'xhs8Pla', name: 'Lemonade'} {sale: 99, full: 129}
-console.log(mergedDrink);             // {id: 'xhs8Pla', name: 'Lemonade', sale: 99, full: 129}
-
-const mergedDrink1 = Object.assign({}, drink, { price });
-console.log(mergedDrink1);            // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 99, full: 129}}
-
-const mergedDrink2 = { ...drink, ...{ price }};  // ... = spread operator
-console.log(mergedDrink2);            // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 99, full: 129}}
+console.log(getType(drink));    // Object
+console.log(getType(null));     // Null
+console.log(getType([]));       // Array
 
