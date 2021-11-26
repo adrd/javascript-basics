9b9fe683e2cf273c1af0fe4a6217f1c273ffd6b0
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 13:31:50 +0200

    Lesson 34 - Object Literal, Function and Constructor Syntax.

--- src/index.js
+++ src/index.js
@@ -5,35 +5,29 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-// 'use strict'
+// literal syntax
+const drink = {
+    id: 'xhs8Pla',
+    name: 'Lemonade',
+    price: 99
+};
 
-const firstCar = { id: 'x8KszK0' };
-const secondCar = { id: 'bc90slqa' };
-const thirdCar = { id: 'h9sNsa' };
+console.log(drink);                            // {id: 'xhs8Pla', name: 'Lemonade', price: 99}
 
-function carPartId(){
-    console.log(this);
-}
+const drinkReference = drink;
+drinkReference.name = 'Peach';
 
-carPartId();                  // undefined, because of using use strict by default by webpack, i can uncomment the code from index.html to see the result
-carPartId.call(firstCar);     // { "id": "x8KszK0" }, attach carPartId() to firstCar object and executes it, then detach it
-carPartId();                  // undefined, there is no default object attached to function hence this is undefined
-carPartId.call(secondCar);    // { "id": "bc90slqa"}, attach carPartId() to secondCar object and executes it, then detach it
-carPartId();                  // undefined, there is no default object attached to function hence this is undefined
+console.log(drink);                            // {id: 'xhs8Pla', name: 'Peach', price: 99}
 
-carPartId.apply(secondCar);   // apply() does the same thing as call(), but call() is the recommended way to do things instead of apply(), 
-                              // if i want to choose between them
+console.log(drink === drinkReference);         // right
 
-function carPartIdNew(name, quantity){
-    console.log(`${this.id}_${name}_${quantity}`);
-}
+console.log({} === {});                        // false
 
-const boundThirdCar = carPartIdNew.bind(thirdCar);  // returns a brand new function with the "this" set to thirdCar, this is the lazy approach
-// console.log(boundThirdCar); 
-boundThirdCar('Windscreen', 99);                    // h9sNsa_Windscreen_99
-boundThirdCar('Exhaust', 9);                        // h9sNsa_Exhaust_9
+// constructor syntax
+console.log(new Object());                     // {}
+// function syntax
+console.log(Object());                         // {}
 
-// call = begins with c = commas
-carPartIdNew.call(firstCar, 'Left Door', 12);       // x8KszK0_Left Door_12
-// apply = begins with a = array
-carPartIdNew.apply(secondCar, ['Right Door', 21]);  // bc90slqa_Right Door_21
+console.log(new Object() instanceof Object);   // true
+console.log(Object() instanceof Object);       // true, internally Object() calls new Object()
+console.log({} instanceof Object)              // true
