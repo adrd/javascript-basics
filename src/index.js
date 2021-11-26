cd70bb9771c5c6ec2b421ebfc95f6fea9609c531
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 11:07:45 +0200

    Lesson 31 - Immediately-Invoked Function Expressions (IIFE).

--- src/index.js
+++ src/index.js
@@ -5,37 +5,17 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-// scope 1
-const anotherID = '1234abcd';
+const carPartId = (function (id) { 
+    // console.log('Hello World!')
 
-function makeCarPartID(id) {
-    // scope 2
-    console.log(id, anotherID);
-
-    // function anotherFunction() {
-    //     // scope 3
-    //     const someID = 99;
-    //     console.log(someID);
-    // }
-}
-
-makeCarPartID('x8YdsZ12');
-
-function makeCarPartIDNew(id) {
     const theId = `CAR_PART_${id}`;
 
     return function(name) {
-        // console.log(theId);
-        return `${theId}_${name.toUpperCase()}`;
-    };
-}
+        return `${theId}_${name}`;
+    }
+        
+})('x7H8sFf');
 
-const carPartId = makeCarPartIDNew('x8YdsZ12');
 console.log(carPartId('Left Door'));
 console.log(carPartId('Right Door'));
-console.log(carPartId('Windscreen'));
-
-const anotherCarPartId = makeCarPartIDNew('7hs9zSaq0');
-console.log(anotherCarPartId('Left Door'));
-console.log(anotherCarPartId('Right Door'));
-console.log(anotherCarPartId('Windscreen'));
+console.log(carPartId('WindScreen'));
