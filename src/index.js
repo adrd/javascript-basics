fe15929b70863a942c8b723f06e00cf3bf0618eb
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 14:25:19 +0200

    Lesson 36 - Shorthand Properties and Methods.

--- src/index.js
+++ src/index.js
@@ -5,26 +5,20 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
+// new to ES2015
+const id = 'xhs8Pla';
+const name = 'Lemonade';
+const price = 99;
+
+const someKey = 'name';
+
 const drink = {
-    id: 'xhs8Pla',
-    name: 'Lemonade',
-    price: 99,
-    getDrinkDetails() {                               // new to ES2015
+    id,
+    [someKey]: name,
+    price,
+    getDrinkDetails() {                               
         return `Drink ${this.name} (${this.price})`;
-    },
-    // getDrinkDetails: () => `Drink ${this.name} (${this.price})`  // arrow function has no 'this' keyword, hence this gives an error, this is undefined
-    'abc 123': 'I am the value!',
-    100: 'I am a number!',
+    }
 };
 
-console.log(drink);                    // {id: 'xhs8Pla', name: 'Lemonade', price: 99, getDrinkDetails: ƒ}
-console.log(drink.getDrinkDetails());  // Drink Lemonade (99)
-console.log(drink['abc 123']);         // I am the value!
-console.log(drink[100]);               // I am a number!
-
-console.log(drink.id);                 // xhs8Pla
-console.log(drink.name);               // Lemonade
-console.log(drink.price);              // 99
-
-const myId = 'id';
-console.log(drink[myId]);              // xhs8Pla
+console.log(drink); // {id: 'xhs8Pla', name: 'Lemonade', price: 99, getDrinkDetails: ƒ}
