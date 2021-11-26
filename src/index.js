c5b745e171d7a70d1badc806a6c2846aff6c4cae
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 13:58:33 +0200

    Lesson 35 - Properties, Methods and Values.

--- src/index.js
+++ src/index.js
@@ -5,29 +5,26 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-// literal syntax
 const drink = {
     id: 'xhs8Pla',
     name: 'Lemonade',
-    price: 99
+    price: 99,
+    getDrinkDetails() {                               // new to ES2015
+        return `Drink ${this.name} (${this.price})`;
+    },
+    // getDrinkDetails: () => `Drink ${this.name} (${this.price})`  // arrow function has no 'this' keyword, hence this gives an error, this is undefined
+    'abc 123': 'I am the value!',
+    100: 'I am a number!',
 };
 
-console.log(drink);                            // {id: 'xhs8Pla', name: 'Lemonade', price: 99}
+console.log(drink);                    // {id: 'xhs8Pla', name: 'Lemonade', price: 99, getDrinkDetails: ƒ}
+console.log(drink.getDrinkDetails());  // Drink Lemonade (99)
+console.log(drink['abc 123']);         // I am the value!
+console.log(drink[100]);               // I am a number!
 
-const drinkReference = drink;
-drinkReference.name = 'Peach';
+console.log(drink.id);                 // xhs8Pla
+console.log(drink.name);               // Lemonade
+console.log(drink.price);              // 99
 
-console.log(drink);                            // {id: 'xhs8Pla', name: 'Peach', price: 99}
-
-console.log(drink === drinkReference);         // right
-
-console.log({} === {});                        // false
-
-// constructor syntax
-console.log(new Object());                     // {}
-// function syntax
-console.log(Object());                         // {}
-
-console.log(new Object() instanceof Object);   // true
-console.log(Object() instanceof Object);       // true, internally Object() calls new Object()
-console.log({} instanceof Object)              // true
+const myId = 'id';
+console.log(drink[myId]);              // xhs8Pla
