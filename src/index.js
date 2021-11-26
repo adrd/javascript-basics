6e628557f446fb60cb0036b4979131f50954c669
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 16:54:13 +0200

    Lesson 42 - Merging Objects.

--- src/index.js
+++ src/index.js
@@ -8,53 +8,21 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 const drink = {
     id: 'xhs8Pla',
     name: 'Lemonade',
-    price: {
-        sale: 99,
-        full: 129
-    },
 };
 
-const drinkReference = drink;
-drinkReference.id = 'abcd';
+const price = {
+    sale: 99,
+    full: 129
+}
 
-console.log(drink);            // {id: 'abcd', name: 'Lemonade', price: {…}}
+const mergedDrink = Object.assign({}, drink, price);
 
-// shallow copies
-const drink1 = {
-    id: 'xhs8Pla',
-    name: 'Lemonade',
-    price: {
-        sale: 99,
-        full: 129
-    },
-};
+console.log(drink, price);            // {id: 'xhs8Pla', name: 'Lemonade'} {sale: 99, full: 129}
+console.log(mergedDrink);             // {id: 'xhs8Pla', name: 'Lemonade', sale: 99, full: 129}
 
-// shallow copies : 2 variants available Object.assign() or ...drink (spread operator)
-// const drinkCloneShallow = Object.assign({}, drink1);   // copies only top level values (either values or references)
-const drinkCloneShallow = {...drink1};                    // copies only top level values (either values or references)
-drinkCloneShallow.id = 'abcd'
-drinkCloneShallow.price.sale = 79;
+const mergedDrink1 = Object.assign({}, drink, { price });
+console.log(mergedDrink1);            // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 99, full: 129}}
 
-console.log(drink1);              // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 79, full: 129}}
-
-// deep copy
-const drink2 = {
-    id: 'xhs8Pla',
-    name: 'Lemonade',
-    price: {
-        sale: 99,
-        full: 129
-    },
-    myFunction() {}
-};
-
-const drinkStringified = JSON.stringify(drink2);
-console.log(drinkStringified);                     // {"id":"xhs8Pla","name":"Lemonade","price":{"sale":99,"full":129}}
-
-const drinkClone2 = JSON.parse(drinkStringified);
-drinkClone2.id = 'abcd'
-drinkClone2.price.sale = 79;
-
-console.log(drinkClone2);                          // {id: 'abcd', name: 'Lemonade', price: {sale: 79, full: 129}} 
-console.log(drink2);                               // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 99, full: 129}, myFunction: ƒ}
+const mergedDrink2 = { ...drink, ...{ price }};  // ... = spread operator
+console.log(mergedDrink2);            // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 99, full: 129}}
 
