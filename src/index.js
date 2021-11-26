47e3e20e20635d7622349197c4d36ea17d9fae5d
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 18:35:14 +0200

    Lesson 44 - Imperative Object Iteration.

--- src/index.js
+++ src/index.js
@@ -14,26 +14,48 @@ const drink = {
     },
 };
 
-// this is not reliable
-console.log(typeof drink);           // object
-console.log(typeof []);              // object
-console.log(typeof null)             // object
-
-// this is not reliable
-console.log({} instanceof Object);   // true
-console.log([] instanceof Object);   // true
-console.log(null instanceof Object); // false
-
-// this is reliable
-console.log(Object.prototype.toString.call(drink));    // [object Object]
-console.log(Object.prototype.toString.call(null));     // [object Null]
-console.log(Object.prototype.toString.call([]));       // [object Array]
-
-function getType(obj) {
-    return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+// for..in loop
+for (const prop in drink) {
+    console.log(prop);             // id, name, price
 }
 
-console.log(getType(drink));    // Object
-console.log(getType(null));     // Null
-console.log(getType([]));       // Array
+for (const prop in drink) {
+    console.log(drink[prop]);       // xhs8Pla, Lemonade, {sale: 99, full: 129}
+}
 
+for (const prop in drink) {
+    const value = drink[prop];
+    //if (typeof value !== 'string') {
+    if (Object.prototype.toString.call(value) === '[object Object]') {
+        for (const key in value) {
+            console.log(key);               // sale, full
+        }
+    }
+}
+
+console.log('------------')
+
+const drink1 = {
+    name: 'Lemonade',
+    price: {
+        sale: 99,
+        full: 129
+    },
+};
+
+const drinkWithId = Object.create(drink1);        // creates a new object having as prototype object drink1 object
+drinkWithId.id = 'xhs8Pla';
+console.log(drinkWithId);
+
+console.log('name' in drinkWithId);               // true
+
+for (const prop in drinkWithId) {
+    console.log(prop);                            // id, name, price, here in operator looks up properties from the prototype object
+}
+
+for (const prop in drinkWithId) {
+    if (drinkWithId.hasOwnProperty(prop)) {       // here we test if the property is indeed on the object itself
+        console.log(prop, drinkWithId[prop]);     // id xhs8Pla        
+    }
+    
+}
