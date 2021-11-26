677448c49161ae726f3a93cb9df8e72af6dd5c07
Adrian Dinu <adrian.dinu@outlook.com>
2021-11-26 16:40:32 +0200

    Lesson 41 - Shallow and Deep Object Cloning.

--- src/index.js
+++ src/index.js
@@ -14,19 +14,47 @@ const drink = {
     },
 };
 
-// slow operation
-// delete drink.id;
-// console.log(drink);    // {name: 'Lemonade', price: {…}}
+const drinkReference = drink;
+drinkReference.id = 'abcd';
 
-// for performance reasons these is the approach you may want to take
-drink.id = undefined;
+console.log(drink);            // {id: 'abcd', name: 'Lemonade', price: {…}}
 
-if (drink.id) {
-    console.log('HAS id...');
-}
+// shallow copies
+const drink1 = {
+    id: 'xhs8Pla',
+    name: 'Lemonade',
+    price: {
+        sale: 99,
+        full: 129
+    },
+};
 
-// console.log(drink.hasOwnProperty('id'));  // need to be mindfull about using hasOwnProperty() function
+// shallow copies : 2 variants available Object.assign() or ...drink (spread operator)
+// const drinkCloneShallow = Object.assign({}, drink1);   // copies only top level values (either values or references)
+const drinkCloneShallow = {...drink1};                    // copies only top level values (either values or references)
+drinkCloneShallow.id = 'abcd'
+drinkCloneShallow.price.sale = 79;
 
-const { price, ...rest } = drink;
-console.log(price, rest, drink);             // {sale: 99, full: 129} {id: undefined, name: 'Lemonade'} {id: undefined, name: 'Lemonade', price: {…}}
+console.log(drink1);              // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 79, full: 129}}
+
+// deep copy
+const drink2 = {
+    id: 'xhs8Pla',
+    name: 'Lemonade',
+    price: {
+        sale: 99,
+        full: 129
+    },
+    myFunction() {}
+};
+
+const drinkStringified = JSON.stringify(drink2);
+console.log(drinkStringified);                     // {"id":"xhs8Pla","name":"Lemonade","price":{"sale":99,"full":129}}
+
+const drinkClone2 = JSON.parse(drinkStringified);
+drinkClone2.id = 'abcd'
+drinkClone2.price.sale = 79;
+
+console.log(drinkClone2);                          // {id: 'abcd', name: 'Lemonade', price: {sale: 79, full: 129}} 
+console.log(drink2);                               // {id: 'xhs8Pla', name: 'Lemonade', price: {sale: 99, full: 129}, myFunction: ƒ}
 
