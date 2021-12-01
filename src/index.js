58bf32026ea6143f5ba464d9ba377f1396f3eeb8
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-01 13:38:23 +0200

    Lesson 50 - Adding Array Elements.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,38 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
 
-const drinks = [["Lemonade", 99], ["Lime", 99], ["Peach", 89]];
+const drinks = ["Lemonade", "Lime", "Peach"];
 
-const [drinkOne, [drinkTwoFirstItem, drinkTwoTwoItem], drinkThird] = drinks;
+// beginning
+drinks.unshift("Water");                     // ['Water', 'Lemonade', 'Lime', 'Peach'], mutable, this function mutates array
 
-console.log(drinkOne, drinkTwoFirstItem, drinkTwoTwoItem, drinkThird); // ['Lemonade', 99] 'Lime' 99 ['Peach', 89]
+const newDrinksArray = ["Pepsi", ...drinks]; // immutable
+// newDrinksArray.length = 0;
+console.log(newDrinksArray);                 // ['Pepsi', 'Water', 'Lemonade', 'Lime', 'Peach']
 
-const [one, [, b], ...rest] = drinks;
+console.log("------------------");
 
-console.log(one, b, rest); // ['Lemonade', 99] 99 [Array(2)] (2) ['Peach', 89]
+// middle
+const index = 1;
+drinks.splice(index, 0, "Mirinda"); // mutable
+console.log(drinks);                // ['Water', 'Mirinda', 'Lemonade', 'Lime', 'Peach']
+
+const newDrinksArrayMiddle = [
+  ...drinks.splice(0, index),       // beginning with 0 index remove 1 item (index value), return the item deleted, 'Water'
+  "Mojito",
+  ...drinks.splice(index - 1),      // from 0 index removes all the items till the end and return all the items removed, 'Mirinda', 'Lemonade', 'Lime', 'Peach'
+]; // immutable
+console.log(newDrinksArrayMiddle);  // ['Water', 'Mojito', 'Mirinda', 'Lemonade', 'Lime', 'Peach']
+
+console.log(drinks); // [], empty array
+
+console.log("------------------");
+
+// end
+drinks.push("Cola"); // mutable, this function mutates array
+drinks.push("Beer");
+
+console.log(drinks); // ['Cola', 'Beer']
+
+const newDrinksArrayEnd = [...drinks, "SevenUp"]; // immutable
+console.log(newDrinksArrayEnd);                   // ['Cola', 'Beer', 'SevenUp']
