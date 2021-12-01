3c8343c5e59de5087f1492485a9cad82d7af3a0a
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-01 11:30:55 +0200

    Lesson 46 - Array Literal, Function and Constructor Syntax.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
-const app = document.getElementById('app');
-app.innerHTML = '<h1>JavaScript Basics</h1>';
+const app = document.getElementById("app");
+app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
 
-const drink = {
-    id: 'xhs8Pla',
-    name: 'Lemonade',
-    price: {
-        sale: 99,
-        full: 129
-    },
-};
+// array literal syntax
+const drinks = ["Lemonade", "Lime", "Peach"];
+const drinksReference = drinks;
 
-console.log(Object.keys(drink));   // keys() function doesn't return properties from the prototype object
+console.log(drinks);
+console.log(drinks === drinksReference); // true
 
-Object.keys(drink).forEach(function (prop, index) {
-    console.log(prop, index);                                      // id 0, name 1, price 2
-});
-
-Object.keys(drink).forEach(function (prop) {
-    console.log(drink[prop]);                                      // xhs8Pla, Lemonade, {sale: 99, full: 129}
-});
-
-Object.keys(drink).forEach(prop => console.log(drink[prop]));      // xhs8Pla, Lemonade, {sale: 99, full: 129}
-
-console.log('-----------')
-
-console.log(Object.entries(drink));   // [Array(2), Array(2), Array(2)], 0: (2) ['id', 'xhs8Pla'], 1: (2) ['name', 'Lemonade'], (2) ['price', {…}]
+// array constructor syntax (avoid using it)
+console.log(new Array("Lemonade", "Lime", "Peach"));
+console.log(Array("Lemonade", "Lime", "Peach")); // under the hood calls new Array()
