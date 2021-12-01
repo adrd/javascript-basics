74b923746510c670a7adfc8b8e9b7cd293792d17
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-01 12:17:21 +0200

    Lesson 48 - Multi-dimensional Arrays.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,17 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
 
-const drinks = ["Lemonade", "Lime", "Peach"];
+const drinks = [["Lemonade", 99], ["Lime", 99], ["Peach", 89]];
 
-console.log(drinks[0]); // Lemonade
+const drinkOne = drinks[0];
+const drinkTwo = drinks[1];
+const drinkThree = drinks[2];
 
-drinks[0] = "Diet Lemonade";
-console.log(drinks[0]); // Diet Lemonade
+console.log(drinkOne);   // ["Lemonade", 99]
+console.log(drinkTwo);   // ["Lime", 99]
+console.log(drinkThree); // ["Peach", 89]
 
-console.log(drinks[drinks.length - 1]); // Peach
+console.log(`Drink: ${drinkOne[0]}, Price: ${drinkOne[1]}`);   // Drink: Lemonade, Price: 99
+console.log(`Drink: ${drinks[0][0]}, Price: ${drinks[0][1]}`); // Drink: Lemonade, Price: 99
 
-// it is good to avoid doing something like this, we are adding a property to array
-// drinks["favourite"] = "Cola";
-// console.log(drinks);
-
-drinks.length = 0;                   // clear items or elements from array
-// drinks.splice(0, drinks.length);  // removes items or elements from array, does the same thing as above
-
-console.log(drinks);
+console.log(drinks);     // (3) [Array(2), Array(2), Array(2)]
