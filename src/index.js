474e5b8682603e23817ba741cf93a13183206849
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-01 12:34:57 +0200

    Lesson 49 - Destructuring Arrays.

--- src/index.js
+++ src/index.js
@@ -7,15 +7,10 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 const drinks = [["Lemonade", 99], ["Lime", 99], ["Peach", 89]];
 
-const drinkOne = drinks[0];
-const drinkTwo = drinks[1];
-const drinkThree = drinks[2];
+const [drinkOne, [drinkTwoFirstItem, drinkTwoTwoItem], drinkThird] = drinks;
 
-console.log(drinkOne);   // ["Lemonade", 99]
-console.log(drinkTwo);   // ["Lime", 99]
-console.log(drinkThree); // ["Peach", 89]
+console.log(drinkOne, drinkTwoFirstItem, drinkTwoTwoItem, drinkThird); // ['Lemonade', 99] 'Lime' 99 ['Peach', 89]
 
-console.log(`Drink: ${drinkOne[0]}, Price: ${drinkOne[1]}`);   // Drink: Lemonade, Price: 99
-console.log(`Drink: ${drinks[0][0]}, Price: ${drinks[0][1]}`); // Drink: Lemonade, Price: 99
+const [one, [, b], ...rest] = drinks;
 
-console.log(drinks);     // (3) [Array(2), Array(2), Array(2)]
+console.log(one, b, rest); // ['Lemonade', 99] 99 [Array(2)] (2) ['Peach', 89]
