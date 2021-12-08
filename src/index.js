443cee308ae9d74e19a78608541395f3497846dc
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-08 12:51:12 +0200

    Lesson 60 - Exploring Array.forEach.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,34 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
 
-const drinks = ["Pepsi", "Lemonade", "Cola"];
+const items = [
+  { id: 1, name: "Super Burger", price: 399 },
+  { id: 2, name: "Jumbo Fries", price: 199 },
+  { id: 3, name: "Big Slurp", price: 299 },
+];
 
-for (let index = 0; index < drinks.length; index++) {
-  const drink = drinks[index];
-  console.log(drink);
-}
+// syntax
+// items.forEach((value, index, array), thisArg => {...})
 
-// new to ES2015
-for (const drink of drinks) {
-  console.log(drink);
-}
+// ["a", "b", "c", "d"].forEach((item, index) => {
+//   console.log(item, index);
+// });
 
-console.log([1, 2, 3]);
+// declarative way
+items.forEach((item) => {
+  app.innerHTML += `
+        <li>
+            ${item.id} ${item.name} ${(item.price / 100).toFixed(2)}
+        </li>
+    `; // toFixed() does rounding
+});
+
+// imperative way
+// for (let index = 0; index < items.length; index++) {
+//   const item = items[index];
+//   app.innerHTML += `
+//         <li>
+//             ${item.id} ${item.name} ${(item.price / 100).toFixed(2)}
+//         </li>
+//     `;
+// }
