d6870e0215bc22c4306f80b9844b5c438aaf64b3
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-08 12:23:27 +0200

    Lessonn 59 - Iteration with for…of.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,15 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 // ----
 
 const drinks = ["Pepsi", "Lemonade", "Cola"];
-let count = 0;
 
 for (let index = 0; index < drinks.length; index++) {
   const drink = drinks[index];
-  if (drink === "Lemonade") {
-    console.log("Pour me a glass!");
-    continue; // count = 2
-    // break; // count = 1
-  }
-  count++;
+  console.log(drink);
 }
 
-console.log(count); // count = 3, without break statement and continue statement
+// new to ES2015
+for (const drink of drinks) {
+  console.log(drink);
+}
+
+console.log([1, 2, 3]);
