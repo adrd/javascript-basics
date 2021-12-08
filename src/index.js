9687fc88fea74474d108a09ddc192a4c042fa891
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-08 12:09:45 +0200

    Lesson 58 - Iteration with Break and Continue.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 // ----
 
 const drinks = ["Pepsi", "Lemonade", "Cola"];
+let count = 0;
 
 for (let index = 0; index < drinks.length; index++) {
   const drink = drinks[index];
   if (drink === "Lemonade") {
     console.log("Pour me a glass!");
+    continue; // count = 2
+    // break; // count = 1
   }
-  // console.log(drinks[index]);
+  count++;
 }
+
+console.log(count); // count = 3, without break statement and continue statement
