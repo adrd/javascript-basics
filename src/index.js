31307116ca818e871ec5cb91cf50d6357baabd1a
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-08 15:43:40 +0200

    Lesson 65 - Exploring Array.every.

--- src/index.js
+++ src/index.js
@@ -6,38 +6,38 @@ app.innerHTML = "<h1>JavaScript Basics</h1>";
 // ----
 
 const items = [
-  { id: 1, name: "Super Burger", price: 399, promo: false },
-  { id: 2, name: "Jumbo Fries", price: 199, promo: false },
-  { id: 3, name: "Big Slurp", price: 299, promo: true },
+  { id: 1, name: "Super Burger", price: 399, stock: true },
+  { id: 2, name: "Jumbo Fries", price: 199, stock: true },
+  { id: 3, name: "Big Slurp", price: 299, stock: false },
 ];
 
 // syntax
-// const returnValue = items.some((value, index, array) => {}, thisArg);
+// const returnValue = items.every((value, index, array) => {...}, thisArg);
 
-// const greaterThanOne = [1, 2, 3].some((x) => x > 1);
-// console.log(greaterThanOne); // true
+// const isEveryValueTrue = [true, true, true].every(Boolean);
+// const isEveryValueTrue = [true, true, true].every((value) => Boolean(value));
+// console.log(isEveryValueTrue); // true
 
 // declarative way
 
-const isInPromo = items.some((item) => item.promo);
-console.log(isInPromo); // true
+const isInStock = items.every((item) => item.stock);
 
 // imperative way
 
-// let isInPromo = false;
+// let isInStock = true;
 
 // for (let index = 0; index < items.length; index++) {
 //   const item = items[index];
-//   if (item.promo) {
-//     isInPromo = true;
+//   if (!item.stock) {
+//     isInStock = false;
 //     break;
 //   }
 // }
-// console.log(isInPromo); // true
 
-const total = isInPromo
-  ? 600
-  : items.reduce((prev, next) => prev + next.price, 0);
-console.log(total); // 600
+if (!isInStock) {
+  console.log(
+    `Sorry, ${items.find((item) => !item.stock).name} is out of stock`
+  );
+}
 
 // console.log(items);
